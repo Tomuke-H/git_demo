2ad42bed238bd89883fe6956cbf88ca8e20d2130
Tom Hohl <tom.hohl@gmail.com>
2021-08-20 11:15:16 -0600

    added comments

--- main.rb
+++ main.rb
@@ -19,3 +19,15 @@ def minus(num1, num2)
 end
 
 puts minus(2,5)
+
+# flow
+# if no repo, run git init to initialize repo (rails does automatically)
+# once you start a project, it's good habit to do init commit
+# git add . 
+# git commit -m'comment'
+# start working on features
+
+# pick a chunk of work, get it working MAKE SURE IT WORKS
+# git add . (or files you want to commit)
+# git commit -m'describe work done'
+# repeat until dead
