9b0de53f41d1811d9c2db7241509546ef46803f5
Tom Hohl <tom.hohl@gmail.com>
2021-08-20 10:59:43 -0600

    added all the things

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
 puts "hello"
+
+# add: stages changes gets them to be commited
+# git add . to stage all changes
+# git add filename to stage individual changes
+
+# git commit -m 'describe your changes'
+
+## create a function that adds two numbers
+def add(num1, num2)
+    return num1 + num2
+end
+
+puts add(2,5)
